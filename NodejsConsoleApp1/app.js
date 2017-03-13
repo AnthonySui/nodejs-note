e03da169be0c767e6c0cc511b77504224d989b0d
Anthony Su <asu@xogrp.com>
2017-03-13 15:52:43 +0800

    add some comment

--- NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/app.js
@@ -1,10 +1,11 @@
-console.log("I will now ask you for your name.");
+//the app shows how to prompt in nodejs
+console.log("will ask what's you name.");
 process.stdin.setEncoding('utf8');
 var name;
 process.stdin.on('readable', function () {
     name = process.stdin.read();
     if (name !== null) {
-        console.log("Hello ".concat(name, ". How are you"));
-        //process.exit();
+        console.log("Hello! ".concat(name, ". How are you"));
+        process.exit();
     }
 });
